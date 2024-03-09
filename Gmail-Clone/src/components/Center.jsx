d965cce1ea64e3ccadaf54b56c5686cadc4f5fc4
saket <saket@saketgupta.com>
2024-03-09 21:31:10 +0530

    center component completed

--- Gmail-Clone/src/components/Center.jsx
+++ Gmail-Clone/src/components/Center.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineStarRate } from "react-icons/md";
+import { GrFormRefresh } from "react-icons/gr";
+
 
 const Center = () => {
   return (
-    <div className=' rounded-2xl w-full'>
-      <h1>this is my center component</h1>
+    <div className=' rounded-2xl w-full p-5'>
+      <GrFormRefresh className='text-[24px] ml-3 mb-2 text-gray-500 hover:bg-gray-300 hover:rounded-xl' />
+      <div className=' flex border-b-2 hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex hover:border hover:shadow-lg  items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex mb-1 hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex mb-1 hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex mb-1 hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex mb-1 hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+      <div className=' border-b-2 flex mb-1 hover:border hover:shadow-lg items-center gap-4 p-2'>
+        <MdOutlineStarRate className='text-[20px] text-gray-500' />
+        <h4 className='text-[17px] font-semibold'>Sender</h4>
+        <h5 className='pl-16 text-gray-500'>Mail ka message yaha likha hoga Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
     </div>
   )
 }
