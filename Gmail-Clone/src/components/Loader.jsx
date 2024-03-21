de7a33acbac6377be99a741dd5217fd7eb34dd85
saket <saket@saketgupta.com>
2024-03-22 00:02:19 +0530

    loader component

---
+++ Gmail-Clone/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div>
+      <h2>this is my loader which will load after some time.</h2>
+    </div>
+  )
+}
+
+export default Loader
