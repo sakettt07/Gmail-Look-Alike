dbe24a9d1a165a9890f483feaa2865bd46b7ae98
saket <saket@saketgupta.com>
2024-03-24 23:59:09 +0530

    setting up tasks and calender functionalities

--- Gmail-Clone/src/components/RightBar.jsx
+++ Gmail-Clone/src/components/RightBar.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import calender from "../assets/images/calender.png";
 import tasks from "../assets/images/tasks.png";
 import contact from "../assets/images/contact.png";
+import Notes from './Notes';
 
 const RightBar = () => {
   return (
     <div className='hidden md:block w-16 h-[100%] bg-[#f6f7fc]'>
       <img className='cursor-pointer w-9 pt-7 pl-3' src={calender} title='calender' alt="" />
       <img className='cursor-pointer w-9 pt-7 pl-3' src={tasks} title='tasks' alt="" />
-      <img className='cursor-pointer w-9 pt-7 pl-3' src={contact} title='contact' alt="" />
+      {/* <img className='cursor-pointer w-9 pt-7 pl-3' src={contact} title='contact' alt="" /> */}
+      <Notes />
     </div>
   )
 }
